68ebeb4c2cf95773fb7cb76e9354625fd7d9dfc5
SheikhRokon <mdrokon.u545@gmail.com>
2023-12-14 14:39:42 +0600

    up date add refresh token

--- user_app/views.py
+++ user_app/views.py
@@ -4,11 +4,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
-from .serializers import *
+from .serializers import UserSerializer, UserLoginSerializer
+
 
 from rest_framework.authtoken.models import Token
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
+
+
+
 
 
 
@@ -18,11 +22,17 @@ class UserRegistrationView(APIView):
     def post(self, request, format=None):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            user = serializer.save()
+
+            # Generate JWT refresh and access tokens
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+
+            # Return success message along with tokens
+            return Response({'message': 'User registered successfully', 'access_token': access_token}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    
+
+
 class UserLoginView(APIView):
     def post(self, request, *args, **kwargs):
         serializer = UserLoginSerializer(data=request.data)
@@ -34,10 +44,17 @@ class UserLoginView(APIView):
 
             if user:
                 refresh = RefreshToken.for_user(user)
-                return Response({'access_token': str(refresh.access_token)}, status=status.HTTP_200_OK)
+                access_token = str(refresh.access_token)
+                refresh_token = str(refresh)
+                
+                return Response({
+                    'message': 'Login successful',
+                    'access_token': access_token,
+                    'refresh_token': refresh_token
+                }, status=status.HTTP_200_OK)
             else:
                 return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
@@ -45,3 +62,7 @@ class UserLoginView(APIView):
 
 def home(request):
     return render(request, 'home.html')
+
+
+
+
